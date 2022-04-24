7688a76f8375e73bf733af35705ebcaf1075c449
nicholasthomas9 <80870490+nicholasthomas9@users.noreply.github.com>
2022-04-24 16:29:40 -0400

    Update main.ts

--- test/main.ts
+++ test/main.ts
@@ -1,12 +1,42 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import ( Handler, Context, Callback } from 'aws-lambda';
+import serverlessExpress from
+"@vendia/segverless-express'i
+import ( NestFactory } from *@nestjs/core':
+import ( INestApplication } from * @nestjs/common' :
+Import AppModule from './app.module' ;
+import ( EventEmitter ) from 'events':
+import ( LoggerService, LogLevel } from '@nestjs/common/services/logger.service':
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+EventEmitter.defaultMaxListeners=0;
+
+
+let cachedServer: Handler:
+
+
+process.on( 'unhandledRejection', (reason) => {
+console.error(reason);
+});
+process.on( 'uncaughtException', (reason) => {
+console.error(reason):
+}):
+
+function getLogger(): LoggerService | LogLevel[] | false {
+  return process.env?.NESTJS_LOGGER === 'true' ? ['error', 'warn', 'log'] : false;
 }
-bootstrap();
+function printRoutes (app: INestApplication)
+const server = app.getHttpServer0):
+const router = server. _events. request._ router:
+const availableRoutes: (1 a router. stack
+.map( (layer) => (.
+if (layer.route)
+return {
+route:{
+path: layer. route?,path,
+method: layer. route?.stack[0] .method,
+},
+};
+}
+})
+.filter((item) => item != undefined);
+console.log(availableRoutes
