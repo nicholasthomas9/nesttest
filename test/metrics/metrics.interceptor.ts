5cfcf21d2e96741ef032d6ac2eb4892646083e0e
imranmunir312 <imranmunir312@gmail.com>
2022-04-25 21:10:35 +0500

    added code to src for logger

--- test/metrics/metrics.interceptor.ts
+++ test/metrics/metrics.interceptor.ts
@@ -1,64 +1,76 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from "@nestjs/common";
 import { Response } from "express";
 import { catchError, map, Observable, tap } from "rxjs";
 import { MetricsService } from "./metrics.service";
 
-
 @Injectable()
 export class MetricsInterceptor implements NestInterceptor {
-    constructor(
-        private readonly metrics: MetricsService,
-    ) { }
+  constructor(private readonly metrics: MetricsService) {}
 
-    async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<any>> {
-        const startTime = new Date();
-        const request = context.switchToHttp().getRequest<Request | any>();
+  async intercept(
+    context: ExecutionContext,
+    next: CallHandler
+  ): Promise<Observable<any>> {
+    const startTime = new Date();
+    const request = context.switchToHttp().getRequest<Request | any>();
 
-        return next.handle().pipe(
-            map(async (data) => {
-                console.log(data);
+    return next.handle().pipe(
+      map(async (data) => {
+        console.log(data);
 
-                const response = context.switchToHttp().getResponse();
-                const requestMetricsData = await this.buildMetricsData(request, response, startTime.getTime());
-                console.log(requestMetricsData);
-
-                this.metrics.setMetricsData(requestMetricsData);
-                await this.metrics.sendMetricsToSqs();
-            }), catchError(async (err) => {
-                const requestMetricsData = this.buildMetricsData(request, err.response, startTime.getTime());
-                console.log(JSON.stringify(requestMetricsData));
-
-                this.metrics.setMetricsData(requestMetricsData);
-                await this.metrics.sendMetricsToSqs();
-                return err.response;
-            })
-
+        const response = context.switchToHttp().getResponse();
+        const requestMetricsData = await this.buildMetricsData(
+          request,
+          response,
+          startTime.getTime()
         );
-    }
+        console.log(requestMetricsData);
 
-    async buildMetricsData(
-        request: Request,
-        response: Response,
-        startTime: number,
-    ): Promise<any> {
-        return new Promise((resolve) => {
-            resolve({
-                endpoint: {
-                    request: {
-                        method: request.method,
-                        endpoint: request.url,
-                    },
-                    response: {
-                        statusCode: response.statusCode,
-                    },
-                    usage: {
-                        startTime,
-                        totalMs: Date.now() - startTime,
-                    },
-                }
-            });
-        });
-        return
-    }
+        this.metrics.setMetricsData(requestMetricsData);
+        await this.metrics.sendMetricsToSqs();
+      }),
+      catchError(async (err) => {
+        const requestMetricsData = this.buildMetricsData(
+          request,
+          err.response,
+          startTime.getTime()
+        );
+        console.log(JSON.stringify(requestMetricsData));
+
+        this.metrics.setMetricsData(requestMetricsData);
+        await this.metrics.sendMetricsToSqs();
+        return err.response;
+      })
+    );
+  }
+
+  async buildMetricsData(
+    request: Request,
+    response: Response,
+    startTime: number
+  ): Promise<any> {
+    return new Promise((resolve) => {
+      resolve({
+        endpoint: {
+          request: {
+            method: request.method,
+            endpoint: request.url,
+          },
+          response: {
+            statusCode: response.statusCode,
+          },
+          usage: {
+            startTime,
+            totalMs: Date.now() - startTime,
+          },
+        },
+      });
+    });
+    return;
+  }
 }
-
--- test/metrics/metrics.service.ts
+++ test/metrics/metrics.service.ts
@@ -1,23 +1,21 @@
 import { Injectable, Scope } from "@nestjs/common";
 
-
 @Injectable()
 export class MetricsService {
-    private metricsData: Record<string, any> = {};
-    
-    setMetricsData(value: unknown, path?: string) {
-        if (path) {
-            console.log(path);
-            
-            // set(this.matricsData, path, value);
-        }else if (typeof value === 'object') {
-            this.metricsData = {...this.metricsData, ...value};
-        }
-    }
+  private metricsData: Record<string, any> = {};
 
+  setMetricsData(value: unknown, path?: string) {
+    if (path) {
+      console.log(path);
 
-    async sendMetricsToSqs(){
-        //not important currently
-        const message = JSON.stringify(this.metricsData);
+      // set(this.matricsData, path, value);
+    } else if (typeof value === "object") {
+      this.metricsData = { ...this.metricsData, ...value };
     }
+  }
+
+  async sendMetricsToSqs() {
+    //not important currently
+    const message = JSON.stringify(this.metricsData);
+  }
 }
